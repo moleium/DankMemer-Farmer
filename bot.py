8ac2b87b3efd7f328700b9c452d795ed0863512b
Mole <93382765+MoleTheDev@users.noreply.github.com>
2022-09-05 00:51:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,92 +1,31 @@
-import discord, os, asyncio
-from discord.ext import commands
-from colorama import Fore, Style
-import random
-import string
-
-token = "your token here"
-
-#Bot prefix, like ?help
+from init import *
+from slashCommands import *
 
 prefix = "?"
 
-#Clear Command
-def Clear():
+client = discord.Client
+client = commands.Bot(command_prefix=prefix, self_bot=True)
 
-    os.system('clear')
-
-#Colors
-def RandomColor():
-    randcolor = discord.Color(random.randint(0x000000, 0xFFFFFF))
-    return randcolor
-
-def RandString():
-    return "".join(random.choice(string.ascii_letters + string.digits) for i in range(random.randint(14, 32)))
-
-# Prefix
-client = discord.Client()
-client = commands.Bot(command_prefix=prefix,self_bot=True)
-
-#D ep
-@client.command(name='dankmemer', aliases=['dank', 'dmc'])
-async def dankmemer(ctx):
-
-    await ctx.message.delete()
-
-    count = 0
-
-    while True:
-
-        try:
-
-            count += 1
-
-            await ctx.send('pls dep max', delete_after=0.1)
-
-            
-
-            await ctx.send('pls beg', delete_after=0.1)
-
-            print(f'{Fore.BLUE}[DANK-MEMER]'+Fore.RESET)
-
-            await asyncio.sleep(45)
-
-        except Exception as e:
-
-            print(f"{Fore.RED}[ERROR]: {Fore.YELLOW}{e}"+Fore.RESET)
-
-#Farm
-@client.command(name='dank-farm', aliases=['dankfarm'])
-async def _fish_dank(ctx): # b'\xfc'
-
-    await ctx.message.delete()
-
-    count = 0
-
-    while True:
-
-        try:
-
-            count += 1
-
-            await ctx.send('pls fish', delete_after=0.1)
-
-            await ctx.send('pls hunt', delete_after=0.1)
-
-            await ctx.send('pls dig', delete_after=0.1)
-
-            print(f'{Fore.BLUE}[AUTO-FARM] {Fore.GREEN}Farm number: {count} sent'+Fore.RESET)
-
-            await asyncio.sleep(46)
-
-        except Exception as e:
-
-            print(f"{Fore.RED}[ERROR]: {Fore.YELLOW}{e}"+Fore.RESET)
-
-           
 @client.event
 async def on_ready():
+    
+    print(f"Loggined in as {client.user.name}\n---")
 
-	print(f"You are Online :)")
+@client.command()
+async def farm(ctx, deposit=False):
 
-client.run(token,bot=False)
+    while True:
+        triggerSlashCommand("fish", dankCommands["fish"], channel_id, guild_id)
+        Hunt(channel_id, guild_id)
+
+        await timeout(5)
+        Beg(channel_id, guild_id)
+        Dig(channel_id, guild_id)
+
+        if deposit == True:
+            await timeout(5)
+            Deposit(channel_id, guild_id)
+
+        await timeout(42)
+
+client.run(token, bot=False)
